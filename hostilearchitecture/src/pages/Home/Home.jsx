bc8c6798039451e2c79728a0a7ab83913d42236a
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-01-23 20:05:40 -0500

    added useNav functionality to direct users to top of page

--- hostilearchitecture/src/pages/Home/Home.jsx
+++ hostilearchitecture/src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Posts from '../../components/Posts/Posts';
 import './home.css'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+  const goToTop = () => {
+    navigate('/#top')
+  }
   return (
     <div id="homePage">
+      <div>
       <Posts />
+      </div>
+      <div id="top-nav">
+        <a href='/#top' onClick={goToTop}
+        >back to top</a>
+
+      </div>
     </div>
   );
 };
